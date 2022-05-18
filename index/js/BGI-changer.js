8b3eca4cc954f75b2a0a2adc232a12442702af2e
Aman mohammed <aman.mohammed979@gmail.com>
2022-05-19 01:28:02 +0530

    fix some bags

--- index/js/BGI-changer.js
+++ index/js/BGI-changer.js
@@ -1,4 +1,4 @@
 $(function () {
-    var imageNo = Math.round((Math.random() * 4) + 1);
+    var imageNo = Math.round((Math.random() * 3) + 1);
     document.getElementById('bgauto').setAttribute("style","background-image: url( ../index/background-img/BG-image-" + imageNo + ".jpg)")
 })
