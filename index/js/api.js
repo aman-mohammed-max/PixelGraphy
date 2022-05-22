466c62cf42b9f879239f9e2400d2eac0f6dd0df2
Aman mohammed <aman.mohammed979@gmail.com>
2022-05-22 15:40:30 +0530

    add api  but it not working

---
+++ index/js/api.js
@@ -0,0 +1,7 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJYZAEdadXIzY2xiN3dhUng1R1F3WGREenVOd3BlcDZAHUTFHQ09YSW1XU0s5bGNDeVd0TG81M1VxUV9DLUtoMExLUzVNYmFWQ3pCWko2bTdfQ0xLYlV4NXRMOW9pU2tpbW11a2c0b3VLTTVLZAE1nXwZDZD'
+});
+userFeed.run();
